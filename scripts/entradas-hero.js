df1aa0e5c9bd3f47e37aa8cae988fcaa1daf119c
unknown <ncguede@gmail.com>
2024-09-11 16:57:32 -0300

    empezando ecco

---
+++ scripts/entradas-hero.js
@@ -0,0 +1,10 @@
+export let entradas = [
+    {
+        "image": "../resc/hero1.webp",
+        "desc-img": "Lucía Jara y Walter Correa"
+    },
+    {
+        "image": "../resc/hero2.webp",
+        "desc-img": "Beneficios para afiliados"
+    }
+]
