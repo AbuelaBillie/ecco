d7d8c9c3f7d5d87f3913345455c4c40175f8235e
AbuelaBillie <ncguede@gmail.com>
2024-11-06 20:06:50 -0300

    avances

--- scripts/header-nav-scroll.js
+++ scripts/header-nav-scroll.js
@@ -2,10 +2,35 @@ window.addEventListener("scroll", () => {
     const header = document.getElementById("header");
     const header_nav_bar = document.getElementById("header-nav-bar");
     const headerHeight = header.offsetHeight;
-
+    const btn_menu = document.getElementById("header-nav-bar-menu");
+    const submenu = document.getElementById("header-nav-bar-submenu");
+    const btn_lupa = document.getElementById("header-nav-bar-lupa");
+    const submenu_lupa = document.getElementById("header-nav-bar-submenu-lupa");
+    
     if (window.scrollY > headerHeight) {
         header_nav_bar.classList.add("visible");
     } else {
         header_nav_bar.classList.remove("visible");
+        submenu.style.display = "none"
+        submenu_lupa.style.display = "none"
     }
+
+    btn_menu.addEventListener("click", ()=>{
+        if (submenu.style.display === "block") {
+            submenu.style.display = "none"
+        }else{
+            submenu_lupa.style.display = "none"
+            submenu.style.display = "block"
+        }
+    })
+
+    btn_lupa.addEventListener("click", ()=>{
+        if (submenu_lupa.style.display === "block") {
+            submenu_lupa.style.display = "none"
+        }else{
+            submenu.style.display = "none"
+            submenu_lupa.style.display = "block"
+        }
+    })
+
 });
